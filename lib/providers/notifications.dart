1454308f092e62020b92af356032f74976ba6823
Gustavo Barros <gustbm78@gmail.com>
2021-11-24 20:58:10 -0300

    asdfvb

--- lib/providers/notifications.dart
+++ lib/providers/notifications.dart
@@ -3,12 +3,14 @@ import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 class Notifications extends ChangeNotifier {
   Future notifyUserOfAcess(String playerId) async {
-    await OneSignal.shared.postNotification(OSCreateNotification(
-        playerIds: [playerId],
-        content: 'Alguém está vendo a sua conta no Carteira Médica agora.',
-        heading: 'Alerta de Acesso',
-        buttons: [
-          OSActionButton(text: "Ok", id: "id"),
-        ]));
+    await OneSignal.shared.postNotification(
+      OSCreateNotification(
+          playerIds: [playerId],
+          content: 'Alguém está vendo a sua conta no Carteira Médica agora.',
+          heading: 'Alerta de Acesso',
+          buttons: [
+            OSActionButton(text: "Ok", id: "id"),
+          ]),
+    );
   }
 }
